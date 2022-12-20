0cc2c5bc2b08a77d7242eb51d166353ffe991401
Juan Ignacio Molina <juanignaciomolina12@gmail.com>
2022-12-20 17:04:50 -0300

    Agregadas funciones de CRUD, Busqueda, Eliminacion y Creacion

--- FutbolApp/urls.py
+++ FutbolApp/urls.py
@@ -4,9 +4,19 @@ from .views import *
 urlpatterns = [
     path('inicio/', inicio, name='inicio'),
     path('padre/', padre, name='padre'),
-    path('jugadores/', jugadores, name='jugadores'),
     path('clubes/', clubes, name='clubes'),
+
+    path('jugadores/', jugadores, name='jugadores'),
     path('jugadoresFormulario/', jugadoresFormulario, name='jugadoresFormulario'),
     path('listarJugadores/', listarJugadores, name='listarJugadores'),
+    path('editarJugador/<id>', editarJugador, name='editarJugador'),
+    path('eliminarJugador/<id>', eliminarJugador, name='eliminarJugador'),
 
+    path('clubesFormulario/', clubesFormulario, name='clubesFormulario'),
+    path('listarClubes/', listarClubes, name='listarClubes'),
+    path('editarClub/<id>', editarClub, name='editarClub'),
+    path('eliminarClub/<id>', eliminarClub, name='eliminarClub'),
+    path('clubesBuscar/', clubesBuscar, name='clubesBuscar'),
+    path('buscar_club/', buscar_club, name='buscar_club'),
 ]
+
--- FutbolApp/views.py
+++ FutbolApp/views.py
@@ -20,7 +20,6 @@ def clubes (request):
 def jugadoresFormulario (request):
     if request.method == 'POST':
         form = PersonaForm(request.POST)
-        
         if form.is_valid():
             informacion = form.cleaned_data
             nombre = informacion["nombre"]
@@ -29,11 +28,10 @@ def jugadoresFormulario (request):
             nacionalidad = informacion["nacionalidad"]
             tamanio = informacion["tamanio"]
             peso = informacion["peso"]
-
             jugador = Persona(nombre=nombre, apellido=apellido, fechaNacimiento=fechaNacimiento, nacionalidad=nacionalidad, tamanio=tamanio, peso=peso)
             jugador.save()
-
-            return render (request, 'FutbolApp/jugadores.html',{"form":form, "mensaje":"Jugador registrado correctamente"})
+            jugadores = Persona.objects.all()
+            return render (request, 'FutbolApp/jugadoresLista.html',{"jugadores":jugadores, "parrafo":"Jugador registrado correctamente"})
         else:
             return render (request, 'FutbolApp/jugadoresFormulario.html',{"form":form, "mensaje":"Error al registrar jugador"})
     else:
@@ -44,4 +42,91 @@ def listarJugadores(request):
     jugadores = Persona.objects.all()
     return render (request, 'FutbolApp/jugadoresLista.html',{"jugadores":jugadores})
 
+def editarJugador(request, id):
+    jugador = Persona.objects.get(id=id)
+    if request.method == 'POST':
+        form = PersonaForm(request.POST)
+        if form.is_valid():
+            info=form.cleaned_data
+            jugador.nombre = info["nombre"]
+            jugador.apellido = info["apellido"]
+            jugador.fechaNacimiento = info["fechaNacimiento"]
+            jugador.nacionalidad = info["nacionalidad"]
+            jugador.tamanio = info["tamanio"]
+            jugador.peso = info["peso"]
+            jugador.save()
+            jugadores = Persona.objects.all()
+            return render (request, 'FutbolApp/jugadoresLista.html',{"jugadores":jugadores, "parrafo":"Jugador editado correctamente"})
+        pass
+    else:
+        form= PersonaForm(initial={"nombre":jugador.nombre, "apellido":jugador.apellido, "fechaNacimiento":jugador.fechaNacimiento, "nacionalidad":jugador.nacionalidad, "tamanio":jugador.tamanio, "peso":jugador.peso})
+        return render (request, 'FutbolApp/editarJugador.html',{"form":form, "jugador":jugador})
+
+def eliminarJugador(request, id):
+    jugador = Persona.objects.get(id=id)
+    jugador.delete()
+    jugadores = Persona.objects.all()
+    return render (request, 'FutbolApp/jugadoresLista.html',{"jugadores":jugadores, "parrafo":"Jugador eliminado correctamente"})
+
+
+def clubesFormulario (request):
+    if request.method == 'POST':
+        form = ClubFutbolForm(request.POST)
+        if form.is_valid():
+            informacion = form.cleaned_data
+            nombre = informacion["nombre"]
+            pais = informacion["pais"]
+            fechaFundacion = informacion["fechaFundacion"]
+            estadio = informacion["estadio"]
+            club = ClubFutbol(nombre=nombre, pais=pais, fechaFundacion=fechaFundacion, estadio=estadio)
+            club.save()
+            clubes = ClubFutbol.objects.all()
+            return render (request, 'FutbolApp/clubesLista.html',{"clubes":clubes, "parrafo":"Club registrado correctamente"})
+        else:
+            return render (request, 'FutbolApp/clubesFormulario.html',{"form":form, "mensaje":"Error al registrar club"})
+    else:
+        formulario = ClubFutbolForm()
+        return render (request, 'FutbolApp/clubesFormulario.html',{"form":formulario})
+
+def listarClubes(request):
+    clubes = ClubFutbol.objects.all()
+    return render (request, 'FutbolApp/clubesLista.html',{"clubes":clubes})
+
+def editarClub(request, id):
+    club = ClubFutbol.objects.get(id=id)
+    if request.method == 'POST':
+        form = ClubFutbolForm(request.POST)
+        if form.is_valid():
+            info=form.cleaned_data
+            club.pais = info["pais"]
+            club.nombre = info["nombre"]
+            club.apodo = info["apodo"]
+            club.colores = info["colores"]
+            club.fundacion = info["fundacion"]
+            club.web = info["web"]
+            club.diminutivo = info["diminutivo"]
+            club.save()
+            clubes = ClubFutbol.objects.all()
+            return render (request, 'FutbolApp/clubesLista.html',{"clubes":clubes, "parrafo":"Club editado correctamente"})
+        pass
+    else:
+        form= ClubFutbolForm(initial={"pais":club.pais, "nombre":club.nombre, "apodo":club.apodo, "colores":club.colores, "fundacion":club.fundacion, "web":club.web, "diminutivo":club.diminutivo})
+        return render (request, 'FutbolApp/editarClub.html',{"form":form, "club":club})
+
+def eliminarClub(request, id):
+    club = ClubFutbol.objects.get(id=id)
+    club.delete()
+    clubes = ClubFutbol.objects.all()
+    return render (request, 'FutbolApp/clubesLista.html',{"clubes":clubes, "parrafo":"Club eliminado correctamente"})
+
+def clubesBuscar(request):
+    return render (request, 'FutbolApp/clubesBuscar.html')
+
+def buscar_club(request):
+    nombre=request.GET["nombre"]
+    if nombre != "":
+        clubes = ClubFutbol.objects.filter(nombre__contains=nombre)
+        return render (request, 'FutbolApp/clubesLista.html',{"clubes":clubes, "parrafo":"Resultado de la busqueda"})
+    else:
+        return render (request, 'FutbolApp/clubesBuscar.html',{"mensaje":"Debe ingresar un nombre"})
 
